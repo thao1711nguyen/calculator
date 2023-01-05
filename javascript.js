002fcecd4192eacb033ccb88bdee1b1d4b2e633a
Thao Nguyen <thao1711nguyen@gmail.com>
2023-01-05 13:31:48 +0700

    Provide functions for the calculator

--- javascript.js
+++ javascript.js
@@ -13,3 +13,87 @@ function divide(...args){
     return args.reduce((total,number) => total/number)
 
 }
+function operate(values){
+    let num1 = Number(values[0]);
+    let num2 = Number(values[2]);
+    let operator = values[1];
+    switch(operator){
+        case '+':
+            return add(num1,num2);
+            break;
+        case '-':
+            return subtract(num1,num2);
+            break;
+        case 'x':
+            return multiply(num1,num2);
+            break;
+        case '/':
+            return divide(num1,num2);
+            break;
+            
+    }
+}
+const buttons = [...document.querySelectorAll('button')];
+const operands= buttons.filter(button => {
+    return (button.textContent!=='CLEAR' && button.textContent!=='=');
+});
+
+const values = []; //store operands & operators
+operands.forEach(button => button.addEventListener('click', function(e) {
+    values.push(e.target.textContent); //store for operation
+    let displayCal = values.reduce((accumulator,value) => accumulator+value);
+    display(displayCal);
+}));
+
+
+//perform calculations
+const equal = document.querySelector('.equal'); 
+equal.addEventListener('click', function() {
+    // let pattern1 = /[x|\/]/;
+    // let pattern2 = /[+|-]/;
+    // if(pattern1.test(values[0])) {
+    //     display('ERROR!');
+    //     return;
+    // }
+
+    // if(pattern2.test(values[0])) { //transform the array
+    //     values[1] = values[0].concat(values[1]);
+    //     values.shift();
+    // }
+    for(let i=0; i < values.length;){
+        let current = Number(values[i]);
+        let next = Number(values[i+1]);
+        if(!isNaN(current) && !isNaN(next)){
+            values[i+1] = values[i].concat(values[i+1]);
+            values.splice(i,1);
+        } else i++;
+    }
+    
+    
+    let answer = 0; //operation
+    while(values.length >=3){
+        answer = operate(values);
+        for(let i=0; i<3; i++){
+            values.shift();
+        }
+        values.unshift(answer);
+    }
+    display(answer);
+});
+
+function display(answer){
+    let displayDiv = document.querySelector('.display'); //display
+    displayDiv.textContent = answer;
+    
+}
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', (e) => {
+    values.length = 0;
+    let displayDiv = document.querySelector('.display'); //display
+    displayDiv.textContent = '';
+
+});
+
+
+
